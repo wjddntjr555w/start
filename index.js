d44d91c5b2c49982959c0e005e60013e201d2a55
wjddntjr555w <wjddntjr555w@naver.com>
2016-01-05 03:09:29 +0900

    MONGOGO

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_DB);
+
+var db = mongoose.connection;
+db.once("open",function(){
+  console.log("DB connected!");
+});
+db.on("error",function(err){
+  console.log("DB ERROR :",err);
+});
+
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data',dataSchema);
+Data.findOne({name:"myData"},function(err,data){
+  console.log('Hi');
+
+  if(err) return console.log('Data ERROR: ',err);
+  if(!data){
+    Data.create({name:"myData",count:0},function(err,data){
+      if(err) return colsole.log("data error: ",err);
+      console.log("Counter Initialized :",data);
+    });
+  }
+});
 
 app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
-var data={count:0};
+
 app.get('/', function(req,res){
-  data.count++;
-  res.render('ejs1',data);
+  Data.findOne({name:"myData"},function(err,data){
+    if(err) return colsole.log('data error:',err);
+    data.count++;
+    data.save(function(err){
+      if(err) return console.log('data error:',err);
+    res.render('ejs1',data);
+  });
+});
 });
 app.get('/reset',function(req,res){
   data.count=0;
