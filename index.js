51391dc70befc7f2d0ce4479bb6319b619c3a51a
wjddntjr555w <wjddntjr555w@naver.com>
2016-01-04 17:01:42 +0900

    second Work

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
-console.log(__dirname);
+var data={count:0};
+app.get('/', function(req,res){
+  data.count++;
+  res.render('ejs1',data);
+});
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('ejs1',data);
+});
+app.get('/set/count', function(req,res){
+  if(req.query.count) data.count=req.query.count;
+  res.render('ejs1',data);
+});
+app.get('/set/:num', function(req,res){
+  data.count=req.params.num;
+  res.render('ejs1',data);
+});
+
 app.listen(3000, function(){
   console.log('server on');
 });
